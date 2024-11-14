339d1d6bb2ea69f96f0ac9cc58c066e98cbb0376
JaoVCarvalho <jaovcarvalho1@gmail.com>
2024-11-14 11:44:41 -0300

    feat: starting with the implementation of the Board and Piece classes

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,18 @@
+package boardgame;
+
+public class Piece {
+
+    protected Position position;
+    private Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+
+        // Por padrão, o Java já iniciaria a variável position como null
+        this.position = null;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
