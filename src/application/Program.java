2072c97022631033bf8f5522dd9ab99f6ca44cfd
JaoVCarvalho <jaovcarvalho1@gmail.com>
2024-11-23 10:24:27 -0300

    feat: add function to clear screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,10 @@
 package application;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPosition;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Program {
@@ -13,14 +15,23 @@ public class Program {
 
         while(true){
 
-            UI.printBoard(chessMatch.getPiece());
-            System.out.println();
-            System.out.print("Source position: ");
-            ChessPosition sourcePosition = UI.readChessPosition(sc);
-            System.out.print("Target position: ");
-            ChessPosition targetPosition = UI.readChessPosition(sc);
-            chessMatch.performChessMove(sourcePosition, targetPosition);
-            System.out.println();
+            try{
+                UI.clearScreen();
+                UI.printBoard(chessMatch.getPiece());
+                System.out.println();
+                System.out.print("Source position: ");
+                ChessPosition sourcePosition = UI.readChessPosition(sc);
+                System.out.print("Target position: ");
+                ChessPosition targetPosition = UI.readChessPosition(sc);
+                chessMatch.performChessMove(sourcePosition, targetPosition);
+                System.out.println();
+            } catch (ChessException e){
+                System.out.println(e.getMessage());
+                sc.nextLine(); // Faz com que o programa espere o usuário apertar a tecla "Enter"
+            } catch (InputMismatchException e){
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
         }
 
     }
