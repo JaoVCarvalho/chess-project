5b1e2bdf89b52c52bc5c2ac51ad5a5ec42ebb278
JaoVCarvalho <jaovcarvalho1@gmail.com>
2024-11-26 20:12:36 -0300

    feat: add king movement

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import board.Board;
+import board.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,8 +16,64 @@ public class King extends ChessPiece {
         return "K";
     }
 
+    private boolean canMove(Position position){
+        return (getBoard().positionExists(position) && (!getBoard().thereIsAPiece(position) || isThereOpponentPiece(position)));
+    }
+
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[0][];
+        boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position auxPosition = new Position(0, 0);
+
+        // Top
+        auxPosition.setValues(position.getRow() - 1, position.getColumn());
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Top Left Diagonal
+        auxPosition.setValues(position.getRow() - 1, position.getColumn() - 1);
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Top Right Diagonal
+        auxPosition.setValues(position.getRow() - 1, position.getColumn() + 1);
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Bottom
+        auxPosition.setValues(position.getRow() + 1, position.getColumn());
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Bottom Left Diagonal
+        auxPosition.setValues(position.getRow() + 1, position.getColumn() - 1);
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Bottom Right Diagonal
+        auxPosition.setValues(position.getRow() + 1, position.getColumn() + 1);
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Right
+        auxPosition.setValues(position.getRow(), position.getColumn() + 1);
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        // Left
+        auxPosition.setValues(position.getRow(), position.getColumn() - 1);
+        if(canMove(auxPosition)){
+            matrix[auxPosition.getRow()][auxPosition.getColumn()] = true;
+        }
+
+        return matrix;
     }
 }
